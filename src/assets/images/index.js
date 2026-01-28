44d51ab1635313df96f74d24b6c4c72d8fcafae0
Agsuser <agsuser@Agsusers-MacBook-Pro.local>
2026-01-28 14:17:16 +0530

    Dynamic logo added in android

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,11 @@
+import Config from 'react-native-config';
+
+const logos = {
+      one: require('./logoOne.png'),
+      two: require('./logoOne.png'),
+      three: require('./logoOne.png'),
+}
+
 export const Images = {
       home: require('./home.png'),
       calendar: require('./calendar.png'),
@@ -6,7 +14,8 @@ export const Images = {
       maillardVilleSplash: require('./maillardVilleSplash.png'),
       veterinary: require('./veterinary.jpg'),
       thirdSplash: require('./thirdSplash.jpg'),
-      
-      
-
+      logoOne: require('./logoOne.png'),
+      logoTwo: require('./logoOne.png'),
+      logoThree: require('./logoOne.png'),
+      logo: logos[Config.APP_FLAVOR] || logos.one,
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,11 @@
+import RNConfig from 'react-native-config';
 import { oneConfic, COLORS as oneColors } from './env.one';
 import { twoConfic, COLORS as twoColors } from './env.two';
 import { threeConfic, COLORS as threeColors } from './env.three';
 import { AppConfig, ThemeColors } from './config.types';
 
 // Options: 'one' | 'two' | 'three'
-export const CURRENT_ENV = 'one';
+export const CURRENT_ENV = RNConfig.APP_FLAVOR || 'one';
 
 const configs: Record<string, { config: AppConfig; colors: ThemeColors }> = {
   one: { config: oneConfic, colors: oneColors },
